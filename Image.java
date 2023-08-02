4afc1f01c5183c702be1b8fcc159e1bcb47f2de7
Parminder Singh <parmindersingh1086@gmail.com>
2023-08-02 18:59:34 -0400

    Added few methods to clear image noise

--- Image.java
+++ Image.java
@@ -1,5 +1,7 @@
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Size;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
@@ -8,9 +10,15 @@ public class Image {
     public static void main(String[] args){
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat img = getImg("testimg1.jpg");
-        Mat newImg = new Mat();
+        Mat tempImg = getImg("testimg1.jpg");
         Imgproc.cvtColor(img, img, Imgproc.COLOR_BGR2GRAY);
-
+        Imgproc.Sobel(img, tempImg, -1, 0, 1, -1);
+        Imgproc.Sobel(img, img, -1, 1, 0, -1);
+        Core.subtract(tempImg, img, img);
+        //Imgproc.blur(img, img, new Size(9,9));
+        //Imgproc.threshold(img, img, 225, 255, 0);
+        //tempImg = dilation(img);
+        showImg(img);
     }
 
     public static Mat getImg(String path){
@@ -20,5 +28,13 @@ public class Image {
     public static void showImg(Mat frame){
         HighGui.imshow("Image", frame);
         HighGui.waitKey();
+    }
+
+    public static Mat dilation(Mat theImg){
+        Mat se = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(100,7));
+        Imgproc.morphologyEx(theImg, theImg,Imgproc.MORPH_CLOSE, se);
+        Imgproc.erode(theImg, theImg, se, new Point(4,4));
+        Imgproc.dilate(theImg, theImg, se, new Point(4,4));
+        return theImg;
     }
 }
