a7c03918328ef402b89c1bcb0ea40c5e654a56b3
Parminder <parmindersingh1086@gmail.com>
2023-08-11 11:29:03 -0400

    added fucntions that can add barcodes found to your drive

--- Image.java
+++ Image.java
@@ -9,46 +9,56 @@ import java.util.List;
 public class Image {
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat img = getImg("testimg1.jpg");
-        Mat outputImg = getImg("testimg1.jpg");
-        img = turnGray(img);
-        img = gradientSubtract(img);
-        img = setPixelThreshold(img);
-        img = dilationAndErosion(img);
-        outputImg = contourFinish(img, outputImg);
+        Mat img = getImg("testimg1.png");
+        Mat outputImg = getImg("testimg1.png");
+        turnGray(img);
+        gradientSubtract(img);
+        setPixelThreshold(img);
+        dilationAndErosion(img);
+        contourFind(img, outputImg);
         showImg(outputImg);
     }
 
+    //This function simple reads in an image from your drive
     public static Mat getImg(String path) {
-        return Imgcodecs.imread("testimg1.jpg");
+        return Imgcodecs.imread("testimg1.png");
     }
 
+    //This function displays the image in a GUI on your computer
     public static void showImg(Mat frame) {
         HighGui.imshow("Image", frame);
         HighGui.waitKey();
     }
 
+    //This function turns image gray which is step one in image processing
     public static Mat turnGray(Mat theImg) {
         Imgproc.cvtColor(theImg, theImg, Imgproc.COLOR_BGR2GRAY);
         return theImg;
     }
 
+    //This function is will sharpen the edges of the image to make it easier to find barcodes
     public static Mat gradientSubtract(Mat theImg) {
         Mat tempX = new Mat();
         Mat tempY = new Mat();
+        //These two functions will find the edges of an image both in vertical and horizontal
         Imgproc.Sobel(theImg, tempX, CvType.CV_64F, 1, 0, -1);
         Imgproc.Sobel(theImg, tempY, CvType.CV_64F, 0, 1, -1);
+        //After we subtract the edges we find with each other leading to an image with higher horizontal gradients
         Core.subtract(tempX, tempY, theImg);
         Core.convertScaleAbs(theImg, theImg);
+        //This blur functions removes any high frequency noise in the image
         Imgproc.blur(theImg, theImg, new Size(9, 9));
         return theImg;
     }
 
+    //In this function we set some rgb values so that only pixels that are white will stand out
     public static Mat setPixelThreshold(Mat theImg) {
         Imgproc.threshold(theImg, theImg, 225, 255, Imgproc.THRESH_BINARY);
         return theImg;
     }
 
+    /*This function removes any random white dots that may still appear on the image, so contour function will not
+    be confused*/
     public static Mat dilationAndErosion(Mat theImg) {
         Mat se = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(100, 7));
         Imgproc.morphologyEx(theImg, theImg, Imgproc.MORPH_CLOSE, se);
@@ -57,19 +67,32 @@ public class Image {
         return theImg;
     }
 
-    public static Mat contourFinish(Mat theImg, Mat result) {
+    //This function finds the contours that are now easily visible after image processing, and draws them on to the img
+    public static Mat contourFind(Mat theImg, Mat result) {
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(theImg, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-        MatOfPoint largestContour = contours.get(0);
-        for (MatOfPoint contour : contours) {
-            if (Imgproc.contourArea(contour) > Imgproc.contourArea(largestContour)) {
-                largestContour = contour;
-            }
-        }
         List<MatOfPoint> finalContours = new ArrayList<>();
-        finalContours.add(largestContour);
-        Imgproc.drawContours(result, finalContours, -1, new Scalar(0, 255, 0), 3);
+        Imgproc.drawContours(result, contours, -1, new Scalar(0, 255, 0), 3);
         return result;
+    }
+
+    //this function is for processing the contours one by one(used in testing)
+    public static Mat drawContour(Mat inputImg, MatOfPoint contour){
+        Mat finalImg = inputImg.clone();
+        List<MatOfPoint> contourList = new ArrayList<>();
+        contourList.add(contour);
+        Imgproc.drawContours(finalImg, contourList, -1, new Scalar(0, 255, 0), 3);
+        return finalImg;
+    }
+
+    //Function is not called, but it will save each contour as its own image on to your computer(used in testing)
+    public static void saveImages(List<MatOfPoint> contours, Mat inputImg){
+        int count = 0;
+        for(MatOfPoint contour : contours){
+            Mat resImg = drawContour(inputImg, contour);
+            String outputFileName = "Contour-" + count + ".png";
+            Imgcodecs.imwrite(outputFileName, resImg);
+        }
     }
 }
